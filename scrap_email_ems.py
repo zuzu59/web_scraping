18be308fb68d1303acdb94efeac83221f99885c8
Christian Zufferey <christian@zufferey.com>
2020-04-08 15:45:52 +0200

    wip

--- scrap_email_ems.py
+++ scrap_email_ems.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Essai de récupération des adresses email sur la descrition des EMS sur https://www.heviva.ch/institutions.html
 
-version = "0.1.1 zf200408.1434"
+version = "0.1.2 zf200408.1544"
 
 print("scrap_email_ems.py ver " + version)
 
@@ -18,10 +18,6 @@ html_txt = html_page.text
 soup = BeautifulSoup(html_txt, 'html.parser')
 #names = soup.findAll("div", { "class" : "focuspoint" })
 names = soup.findAll("a", { "class" : "item" })
-
-
-#                 WIP
-
 
 
 
@@ -62,36 +58,7 @@ def scrap_email():
     
     
     
-    # criminals = {}
-    # for criminal in names :
-    #     url_personne = criminal.attrs['data-base-url'].split('@@')[0]
-    #     name = criminal.img.attrs['alt']
-    #     criminals[name] = url_personne
-    # return criminals    
-    
 
-
-# def getCriminal_name(criminal):
-#     return(name)
-# 
-# def getCriminal_detail(criminal):
-#     url_personne = criminal.attrs['data-base-url'].split('@@')[0]
-#     return(url_personne)
-# 
-# def getCriminals():
-#     return(names)
-# 
-# def getCriminalsList():
-#     criminals = {}
-#     for criminal in names :
-#         url_personne = criminal.attrs['data-base-url'].split('@@')[0]
-#         name = criminal.img.attrs['alt']
-#         criminals[name] = url_personne
-#     return criminals    
-
-
-    #print(url_personne.split('@@')[0])
-#url_personne = names[0].attrs['data-base-url']
 if __name__ == '__main__':
 #    print(getCriminals())
     disp_debug()
